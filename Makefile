d3a87ee0acdf7b06b6f91cdfc592cf800985a892
LEGHERABA Mohamed <3300273@pc4021.polytech.upmc.fr>
2017-03-12 23:25:48 +0100

    Version MPI a testé

--- Makefile
+++ Makefile
@@ -1,21 +1,30 @@
-target: decideMP
+target: decimeMPI decideMP decide
 CC = gcc
+MCC = mpicc
 CFLAGS=-fopenmp -O3 -std=c99
 
 mainMP.o:mainMP.c projet.h
 	$(CC) $(CFLAGS) -c mainMP.c 
+mainMPI.o:mainMPI.c projet.h
+	$(MCC) -c mainMPI.c
 main.o:mainMP.c projet.h
 	$(CC) -O3 -std=c99 -c main.c
 aux.o: aux.c projet.h
 	$(CC) $(CFLAGS) -c aux.c
 
+
+
 decideMP: mainMP.o aux.o
 	$(CC) $(CFLAGS) $^ -o $@
-
+decideMPI: mainMPI.o aux.o
+	$(MCC) -O3 -std=c99 $^ -o $@
 decide: main.o aux.o
 	$(CC) -O3 -std=c99 $^ -o $@
+
+
+
 .PHONY: clean
 
 clean:
-	rm -f *.o decideMP decide
+	rm -f *.o decide MPI decideMP decide
 
--- mainMPI.c
+++ mainMPI.c
@@ -75,7 +75,106 @@ void evaluate(tree_t * T, result_t *result)
           tt_store(T, result);
 }
 
+void traitement_eval(tree_t T,move_t moves,result_t result,int rank)
+{
+	tree_t child;
+    result_t child_result;
+    
+    play_move(T, moves[rank], &child);
+    
+    evaluate(&child, &child_result);
+             
+    int child_score = -child_result.score;
+	
+	if (child_score > result->score) {
+		result->score = child_score;
+		result->best_move = moves[i];
+    	result->pv_length = child_result.pv_length + 1;
+     	for(int j = 0; j < child_result.pv_length; j++)  result->PV[j+1] = child_result.PV[j];
+        result->PV[0] = moves[i];
+    }
 
+    if (ALPHA_BETA_PRUNING && child_score >= T->beta)
+      break;    
+
+    T->alpha = MAX(T->alpha, child_score);
+}
+
+void evaluatePara(tree_t * T, result_t *result)
+{
+			int my_rank,p;
+		int maitre = 0;
+		
+		MPI_Status status;
+
+		MPI_Comm_size(MPI_COMM_WORLD, &p);
+		MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
+		
+        node_searched++;
+  
+        move_t moves[MAX_MOVES];
+        int n_moves;
+
+        result->score = -MAX_SCORE - 1;
+        result->pv_length = 0;
+        
+        if (test_draw_or_victory(T, result))
+          return;
+
+        if (TRANSPOSITION_TABLE && tt_lookup(T, result))     /* la réponse est-elle déjà connue ? */
+          return;
+        
+        compute_attack_squares(T);
+
+        /* profondeur max atteinte ? si oui, évaluation heuristique */
+        if (T->depth == 0) {
+          result->score = (2 * T->side - 1) * heuristic_evaluation(T);
+          return;
+        }
+        
+        n_moves = generate_legal_moves(T, &moves[0]);
+
+        /* absence de coups légaux : pat ou mat */
+	if (n_moves == 0) {
+          result->score = check(T) ? -MAX_SCORE : CERTAIN_DRAW;
+          return;
+        }
+        
+        if (ALPHA_BETA_PRUNING)
+          sort_moves(T, n_moves, moves);
+
+			/* On envoie un child à chaque processeurs puis ceux qui restent*/
+		
+		if(n_moves<=p)
+		{
+			if(my_rank<n_moves)
+			{
+				traitement_eval(T,moves,result,my_rank);	
+			}
+		}
+		else
+		{
+			int mult = n_moves/p;
+			int nmov_restant = n_moves%p;
+			for(int i=0; i<mult;i++)
+			{
+				traitement_eval(T,moves,result,my_rank+p*i);
+			}
+			if(my_rank<nmov_restant)
+			{
+				traitement_eval(T,moves,result,my_rank+p*mult);
+			}
+		
+		}
+		// A tester
+		MPI_Allreduce(result->score,result->score,1,MPI_INT,MPI_MIN,MPI_COMM_WORLD)
+
+		
+		if (TRANSPOSITION_TABLE)
+          tt_store(T, result);
+
+
+}
 void decide(tree_t * T, result_t *result)
 {
 	for (int depth = 1;; depth++) {
@@ -86,7 +185,7 @@ void decide(tree_t * T, result_t *result)
 
                 printf("=====================================\n");
 
-		evaluate(T, result);
+		evaluatePara(T, result);
 
                 printf("depth: %d / score: %.2f / best_move : ", T->depth, 0.01 * result->score);
                 print_pv(T, result);
@@ -98,11 +197,9 @@ void decide(tree_t * T, result_t *result)
 
 int main(int argc, char **argv)
 {  
-	int my_rank;
 
-	MPI_Status status;
 	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
+
 	double debut ,fin;
 	tree_t root;
         result_t result;
